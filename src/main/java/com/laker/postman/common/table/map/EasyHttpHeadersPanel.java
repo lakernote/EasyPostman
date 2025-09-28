8aaf9b608dae9649e05f46dfc0fb4a05ed337905
laker <935009066@qq.com>
2025-09-28 12:54:10 +0800

    feat: add EasyHttpHeadersPanel for managing HTTP headers with a customizable table

---
+++ src/main/java/com/laker/postman/common/table/map/EasyHttpHeadersPanel.java
@@ -0,0 +1,14 @@
+package com.laker.postman.common.table.map;
+
+import javax.swing.*;
+
+/**
+ * 高仿Postman的Headers面板
+ * 1. 三列表格，第一列启用开关列(固定宽度，默认勾选)，第二列Name，第三列Value
+ * 2.左上角有Headers标签和eye图标按钮，点击可切换显示/隐藏
+ * 3.中间是表格，支持增删行
+ * 4.默认请求头Accept和User-Agent不可删除
+ */
+public class EasyHttpHeadersPanel extends JPanel {
+
+}

8873e9f8ce6b973b24ecf226d680421838bb7ca1
laker <935009066@qq.com>
2026-03-05 14:23:54 +0800

    chore: bump version to 4.3.43 in pom.xml

--- src/main/java/com/laker/postman/panel/toolbox/ToolboxPanel.java
+++ src/main/java/com/laker/postman/panel/toolbox/ToolboxPanel.java
@@ -130,6 +130,7 @@ public class ToolboxPanel extends SingletonBasePanel {
         // 数据库
         reg("es", MessageKeys.TOOLBOX_ELASTICSEARCH, "icons/database.svg", GRP_DB, new ElasticsearchPanel());
         reg("influxdb", MessageKeys.TOOLBOX_INFLUXDB, "icons/database.svg", GRP_DB, new InfluxDbPanel());
+        reg("kafka", MessageKeys.TOOLBOX_KAFKA, "icons/database.svg", GRP_DB, new KafkaPanel());
         // 开发
         reg("diff", MessageKeys.TOOLBOX_DIFF, "icons/file.svg", GRP_DEV, new DiffPanel());
         reg("decompiler", MessageKeys.TOOLBOX_DECOMPILER, "icons/decompile.svg", GRP_DEV, new DecompilerPanel());

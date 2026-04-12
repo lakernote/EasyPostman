08604f21d7e22df1aa8e8c916020b70f099139cf
laker <935009066@qq.com>
2026-04-12 19:53:46 +0800

    feat: add tests for OkHttpResponseHandler and HttpRequestFactory, enhance user agent handling

--- easy-postman-app/src/main/java/com/laker/postman/service/http/HttpRequestFactory.java
+++ easy-postman-app/src/main/java/com/laker/postman/service/http/HttpRequestFactory.java
@@ -5,8 +5,10 @@ import com.laker.postman.model.HttpHeader;
 import com.laker.postman.model.HttpRequestItem;
 import com.laker.postman.model.RequestItemProtocolEnum;
 import com.laker.postman.panel.collections.right.request.sub.RequestBodyPanel;
+import com.laker.postman.util.SystemUtil;
 import lombok.experimental.UtilityClass;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,14 +17,25 @@ import static com.laker.postman.service.collections.DefaultRequestsFactory.CONTE
 
 @UtilityClass
 public class HttpRequestFactory {
+    private static final String DEFAULT_USER_AGENT_VERSION = "dev";
     public static final String TEXT_EVENT_STREAM = "text/event-stream";
     public static final String ACCEPT = "Accept";
     public static final String USER_AGENT = "User-Agent";
-    public static final String EASY_POSTMAN_CLIENT = "EasyPostman Client";
+    public static final String EASY_POSTMAN_CLIENT = "EasyPostman/" + resolveUserAgentVersion();
     public static final String ACCEPT_ENCODING = "Accept-Encoding";
     public static final String CONNECTION = "Connection";
     public static final String ACCEPT_ENCODING_VALUE = "gzip, deflate, br";
     public static final String CONNECTION_VALUE = "keep-alive";
+
+    private static String resolveUserAgentVersion() {
+        String version = SystemUtil.getCurrentVersion();
+        if (version == null || version.isBlank()) {
+            return DEFAULT_USER_AGENT_VERSION;
+        }
+        return StandardCharsets.US_ASCII.newEncoder().canEncode(version)
+                ? version
+                : DEFAULT_USER_AGENT_VERSION;
+    }
 
     public static HttpRequestItem createDefaultRequest() {
         // Create a default test request

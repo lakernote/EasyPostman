ea989db52a83db6d102d9b6ac8509b724790db40
laker <935009066@qq.com>
2026-06-02 21:34:27 +0800

    test: prevent uncaught exception dialog in desktop tests

--- easy-postman-app/src/test/java/com/laker/postman/startup/AppUncaughtExceptionHandlerTest.java
+++ easy-postman-app/src/test/java/com/laker/postman/startup/AppUncaughtExceptionHandlerTest.java
@@ -1,32 +1,27 @@
 package com.laker.postman.startup;
 
 import org.testng.annotations.Test;
+import org.testng.SkipException;
 
+import java.awt.GraphicsEnvironment;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class AppUncaughtExceptionHandlerTest {
-
     @Test
     public void shouldNotShowSwingDialogWhenHeadless() throws Throwable {
-        String previousHeadless = System.getProperty("java.awt.headless");
-        System.setProperty("java.awt.headless", "true");
-        try {
-            AppUncaughtExceptionHandler handler = new AppUncaughtExceptionHandler();
-            Method notifyUser = AppUncaughtExceptionHandler.class.getDeclaredMethod("notifyUser");
-            notifyUser.setAccessible(true);
+        if (!GraphicsEnvironment.isHeadless()) {
+            throw new SkipException("Only verifies the headless branch; desktop runs would show a real dialog");
+        }
 
-            try {
-                notifyUser.invoke(handler);
-            } catch (InvocationTargetException e) {
-                throw e.getCause();
-            }
-        } finally {
-            if (previousHeadless == null) {
-                System.clearProperty("java.awt.headless");
-            } else {
-                System.setProperty("java.awt.headless", previousHeadless);
-            }
+        AppUncaughtExceptionHandler handler = new AppUncaughtExceptionHandler();
+        Method notifyUser = AppUncaughtExceptionHandler.class.getDeclaredMethod("notifyUser");
+        notifyUser.setAccessible(true);
+
+        try {
+            notifyUser.invoke(handler);
+        } catch (InvocationTargetException e) {
+            throw e.getCause();
         }
     }
 }
